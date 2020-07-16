05a253b0781d60e9494e452575031605d6d06742
FaroukDev <fa.elayache1@gmail.com>
2020-07-16 16:17:42 +0200

    add component TablePerso and call to api on index.js

--- app/containers/PersonnagesPage/index.js
+++ app/containers/PersonnagesPage/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable prettier/prettier */
 /*
  * FeaturePage
  *
@@ -7,20 +9,58 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
+
 import H1 from 'components/H1';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 import messages from './messages';
+import TablePerso from './TablePerso';
+
+const myheader = new Headers({
+  'Content-Type' : 'application/x-www-form-urlencoded',
+})
+
+const init = {
+  method : 'GET',
+  headers: myheader,
+  mode: 'cors'
+}
 
 export default class PersonnagesPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchName: '',
+      searchName: 'a',
+      persos: []
     };
   }
 
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
+  componentDidMount = () => {
+    this.handleCallAPIPerso('spider')
+  }
+
+  handleChange = event => {
+    this.setState({ searchName: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.handleCallAPIPerso(this.state.searchName);
+  }
+
+  handleCallAPIPerso = (name)=> {
+    const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&apikey=b3ee1b80a4329537128049d2fa454a0d`;
+    fetch(url, init)
+      .then(response => response.json())
+      .then(json => {
+        const data = json;
+        // eslint-disable-next-line no-console
+        console.log("data-api:", data.data.results);
+        this.setState({persos: data.data.results })
+        
+      })
+      .catch(error => console.log(error));
+      
   };
 
   render() {
@@ -43,6 +83,14 @@ export default class PersonnagesPage extends React.Component {
           onChange={this.handleChange}
           margin="normal"
         />
+        <Button 
+          variant="contained" 
+          color="primary"
+          onClick = {this.handleSearch}>
+          Rechercher
+        </Button>
+        <Checkbox />
+        <TablePerso persos={this.state.persos}/>
       </div>
     );
   }
